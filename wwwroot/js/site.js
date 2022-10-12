2ed4d94a2fa02f53a41dc13c347438d5d9930c0f
GUIDO SCHULMAN <46961979@ort>
2022-10-12 12:02:19 -0300

    .

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,12 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function CargarDatos(){
+    const array = Restaurante
+    Restaurante.forEach(element => {
+    console.log(element);
+   });
+}
+
+
+
